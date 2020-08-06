8c8dbd1c33d30e43d4756123d8ea47c4e25bf01a
Elias Riveros <elias4845@gmail.com>
2020-08-05 20:18:46 -0400

    marcebot.py file created.

---
+++ marcebot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '$')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! ({round(client.latency * 1000)}ms)')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.",
+				"It is decidedly so.",
+				"Without a doubt.",
+				"Yes - definitely.",
+				"You may rely on it.",
+				"As I see it, yes.",
+				"Most likely.",
+				"Outlook good.",
+				"Yes.",
+				"Signs point to yes.",
+				"Reply hazy, try again.",
+				"Ask again later.",
+				"Better not tell you now.",
+				"Cannot predict now.",
+				"Don't count on it.",
+				"My reply is no.",
+				"My sources say no.",
+				"Outlook not so good.",
+				"Very doubtful."]
+	await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+client.run('NzQwNzE5ODUzMDIyODcxNjMz.XytHHg.vRsC3n-f966rGwisAYrwADz4OYE')
