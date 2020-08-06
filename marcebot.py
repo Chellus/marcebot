4c0699ee23fb9ef61653cfe6fa2c30dabfb8f5e8
Elias Riveros <elias4845@gmail.com>
2020-08-05 20:30:11 -0400

    8ball command added.

--- marcebot.py
+++ marcebot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = '$')
 
@@ -11,27 +12,27 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'Pong! ({round(client.latency * 1000)}ms)')
 
-@client.command(aliases=['8ball'])
+@client.command(aliases=['8ball']):
 async def _8ball(ctx, *, question):
-    responses = ["It is certain.",
-				"It is decidedly so.",
-				"Without a doubt.",
-				"Yes - definitely.",
-				"You may rely on it.",
-				"As I see it, yes.",
-				"Most likely.",
-				"Outlook good.",
-				"Yes.",
-				"Signs point to yes.",
-				"Reply hazy, try again.",
-				"Ask again later.",
-				"Better not tell you now.",
-				"Cannot predict now.",
-				"Don't count on it.",
-				"My reply is no.",
-				"My sources say no.",
-				"Outlook not so good.",
-				"Very doubtful."]
-	await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+    responses = ['It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes - definetly',
+    'You may relay on it.',
+    'As i see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Do not count on it.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 client.run('NzQwNzE5ODUzMDIyODcxNjMz.XytHHg.vRsC3n-f966rGwisAYrwADz4OYE')
