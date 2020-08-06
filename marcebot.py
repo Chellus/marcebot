633579cdee88b77b7ebd7dde3ef6fd10e0229844
Elias Riveros <elias4845@gmail.com>
2020-08-05 21:06:15 -0400

    Kick, ban and unban commands added.

--- marcebot.py
+++ marcebot.py
@@ -35,4 +35,32 @@ async def _8ball(ctx, *, question):
     'Very doubtful.']
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'{user} has been kicked succesfully.')
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await ctx.send(f'{user} has been banned succesfully.')
+    await member.ban(reason=reason)
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator == member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+        else:
+            await ctx.send(f'The user {member_name}#{member_discriminator} is not banned.')
+
+
 client.run('NzQwNzE5ODUzMDIyODcxNjMz.XytHHg.vRsC3n-f966rGwisAYrwADz4OYE')
